6d6ca1fdb30d8dc36d82020b01dbb0e5f64fe7a9
bixash <ibkstha@gmail.com>
2023-06-29 22:33:33 +0545

    login, signup, base routes added

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
-dict = {}
-data = {1,2,3,34,5,5,654,646,546,5}
-for row in data:
-    dict.update({"1": row})
-    # print("1")
-print(dict)
+# dict = {}
+# data = {1,2,3,34,5,5,654,646,546,5}
+# for row in data:
+#     dict.update({"1": row})
+#     # print("1")
+# print(dict)
+
+import sqlite3 as sql
+
+con = sql.connect("database.db", check_same_thread=False)
+cur = con.cursor()
+
+email = 'bikash@gmail.com'
+password = 'password'
+
+cur.execute("SELECT * FROM `User` where email= ? and password = ?", (email, password))
+result = cur.fetchone()
+print(result[1])
--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 import sqlite3 as sql
 
 con = sql.connect("database.db", check_same_thread=False)
@@ -8,8 +8,33 @@ views = Blueprint('views', __name__)
 
 
 @views.route('/')
+def base():
+   return render_template("base.html")
+
+
+@views.route('/login', methods=['GET', 'POST'])
+def login():
+   if request.method == 'POST':
+      email = request.form['email']
+      password = request.form['password']
+
+      cur.execute("SELECT * FROM `User` where email= ? and password = ?", (email, password))
+      result = cur.fetchone()
+
+      if email == result[1] and password == result[2]:
+         return render_template("dashboard.html", user_id = result[0])
+      
+   return render_template("login.html")
+
+
+@views.route('/signup', methods=['GET', 'POST'])
+def signup():
+   return render_template("signup.html")
+
+
+@views.route('/dashboard')
 def dashboard():
-   
+
    cur.execute("SELECT DISTINCT scrip FROM `Transaction`")
    stock_symbols = cur.fetchall()
 
