73dff33680a237966a25f118d5406b7d509bcf7c
bixash <ibkstha@gmail.com>
2023-07-14 08:49:27 +0545

    username column added

--- schema.py
+++ schema.py
@@ -9,7 +9,7 @@ def create_user_table():
 
 # Schema for transaction table
 def create_transaction_table():
-    cur.execute("CREATE TABLE transactions (id INTEGER NOT NULL, scrip VARCHAR(225),transaction_date DATE NOT NULL, credit_quantity INTEGER NOT NULL, debit_quantity INTEGER NOT NULL,balance_after_transaction INTEGER NOT NULL,history_description VARCHAR(225), uid INTEGER NOT NULL, FOREIGN KEY(uid) REFERENCES user(id),FOREIGN KEY(scrip) REFERENCES stock(scrip), PRIMARY KEY (id))")
+    cur.execute("CREATE TABLE transactions (id INTEGER NOT NULL, scrip VARCHAR(225),transaction_date DATE NOT NULL, credit_quantity INTEGER NOT NULL, debit_quantity INTEGER NOT NULL,balance_after_transaction INTEGER NOT NULL,history_description VARCHAR(225), uid INTEGER NOT NULL, FOREIGN KEY(uid) REFERENCES user(id), FOREIGN KEY(scrip) REFERENCES stock(scrip), PRIMARY KEY (id))")
 
 def create_stock_table():
     cur.execute("CREATE TABLE stock(scrip VARCHAR(255), closing_price INTEGER, PRIMARY KEY (scrip))")
