704afc5ff40b1132974f97ba9a1d93cb813b15c3
bixash <ibkstha@gmail.com>
2023-07-21 21:12:30 +0545

    moved main.py file from api

---
+++ main.py
@@ -0,0 +1,41 @@
+import logging
+
+import uvicorn, os
+from fastapi import FastAPI, Request
+from starlette.middleware.cors import CORSMiddleware
+
+from portfoliotracker.api import auth_router
+from portfoliotracker.entities import BaseResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+app = FastAPI(title="Stock Portfolio Tracker")
+
+logger = logging.getLogger(__name__)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Mount the static files (css, js, etc.) from the resources/static directory
+app.mount("/static", StaticFiles(directory=os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources", "static")), name="static")
+
+templates_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources", "templates")
+templates = Jinja2Templates(directory=templates_directory)
+
+
+@app.get("/")
+async def root(request: Request):
+    # return BaseResponse(error=False, msg="OK")
+    context = {"name": "World"}
+    return templates.TemplateResponse("base.html",{ "request": request, "context": context})
+
+
+app.include_router(auth_router.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
