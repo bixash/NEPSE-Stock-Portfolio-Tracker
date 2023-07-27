ba3d24ce46e87853101825de75ca9620f7b52cad
bixash <ibkstha@gmail.com>
2023-07-27 22:15:32 +0545

    portfolio_router include

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI, Request
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 
-from portfoliotracker.api import auth_router, transaction_router, upload_router
+from portfoliotracker.api import auth_router, transaction_router, upload_router, portfolio_router
 from portfoliotracker.entities import BaseResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
@@ -45,6 +45,7 @@ async def root(request: Request):
 app.include_router(auth_router.router)
 app.include_router(upload_router.router)
 app.include_router(transaction_router.router)
+app.include_router(portfolio_router.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload  = True)
