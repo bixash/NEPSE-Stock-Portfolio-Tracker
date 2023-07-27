47a3a39ce0f506e3c7999877eedff86e3fcdc821
bixash <ibkstha@gmail.com>
2023-07-27 17:13:00 +0545

    transaction class

---
+++ portfoliotracker/entities/transaction.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+
+
+class Transaction(BaseModel):
+    scrip: str = None
+    transaction_date: str = None
+    credit_quantity: int = None
+    debit_quantity: int = None
+    balance_after_transaction: int = None
+    history_description: str = None
