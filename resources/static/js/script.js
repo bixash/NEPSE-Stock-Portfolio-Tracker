ebc7f7aae6ac9c13d9a0721f149f9091b845db6b
bixash <ibkstha@gmail.com>
2024-04-01 22:40:06 +0545

    chart.js & jquery file removed

--- resources/static/js/script.js
+++ resources/static/js/script.js
@@ -1,50 +1,45 @@
-$(document).ready(function () {
-  $.get("/transactions/sector-stats", function (response, status) {
-    array = response.result;
-    
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/transactions/sector-stats")
+    .then(response => response.json())
+    .then(data => {
+      const xValues = data.result.xValues;
+      const yValues = data.result.yValues;
 
-    var xValues = array.xValues;
-    var yValues = array.yValues;
+      const barColors = [
+        "rgba(0, 0, 255, 1.0)",
+        "rgba(0, 0, 255, 0.8)",
+        "rgba(0, 0, 255, 0.6)",
+        "rgba(0, 0, 255, 0.4)",
+        "rgba(0, 0, 255, 0.2)",
+        "rgba(50, 0, 255, 1.0)",
+        "rgba(50, 0, 255, 0.8)",
+        "rgba(50, 0, 255, 0.6)",
+        "rgba(50, 0, 255, 0.4)",
+        "rgba(50, 0, 255, 0.2)",
+        "rgba(100, 0, 255, 1.0)",
+        "rgba(100, 0, 255, 0.6)",
+        "rgba(100, 0, 255, 0.4)",
+      ];
 
-    var barColors = [
-      "rgba(0,0,255,1.0)",
-      "rgba(0,0,255,0.8)",
-      "rgba(0,0,255,0.6)",
-      "rgba(0,0,255,0.4)",
-      "rgba(0,0,255,0.2)",
-      "rgba(50,0,255,1.0)",
-      "rgba(50,0,255,0.8",
-      "rgba(50,0,255,0.6)",
-      "rgba(50,0,255,0.4)",
-      "rgba(50,0,255,0.2)",
-      "rgba(100,0,255,1.0)", 
-      "rgba(100,0,255,0.6)", 
-      "rgba(100,0,255,0.4)",
-      
-   
-     
-    ];
-
-    new Chart("sectorChart", {
-      type: "doughnut",
-      data: {
-        labels: xValues,
-        datasets: [
-          {
-            backgroundColor: barColors,
-            data: yValues,
-          },
-        ],
-      },
-      options: {
-        title: {
-          display: false,
+      new Chart("sectorChart", {
+        type: "doughnut",
+        data: {
+          labels: xValues,
+          datasets: [
+            {
+              backgroundColor: barColors,
+              data: yValues,
+            },
+          ],
         },
-      },
+        options: {
+          title: {
+            display: false,
+          },
+        },
+      });
+    })
+    .catch(error => {
+      console.error("Error fetching data:", error);
     });
-  });
 });
-
-
-
-
